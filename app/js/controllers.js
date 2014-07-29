db1707e5102532d34a34ea2b507120853b31149a
prasana-cuelogic <prasanaa.cuelogic@gmail.com>
2014-07-29 17:23:41 +0530

    Commit angular seed folder setup

---
+++ app/js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+var app = angular.module('myApp.controllers', []);
+app.controller('MyCtrl1', ['$scope', function($scope) {
+
+  }])
+app.controller('MyCtrl2', ['$scope', function($scope) {
+
+  }]);
