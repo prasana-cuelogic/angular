bfffb282389c00e581aa43c382dcb2d88f54cd76
prasana-cuelogic <prasanaa.cuelogic@gmail.com>
2014-08-12 18:47:40 +0530

    commit files for assignment3

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,17 +3,21 @@
 /* Directives */
 
 
-angular.module('myAngularApp.directives', []).
+objApp = angular.module('myAngularApp.directives', []).
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]).
-  directive ('ngHeader',function(){
-  	return function(){
-  		
+  }]);
+  objApp.directive ('ngHeader',function(){
+  	return{
+  		restrict: 'A',
+    	templateUrl: 'partials/partial1.html'
   	}
-  })
-  ;
-
-
+  });
+  objApp.directive ('footer',function(){
+  	return {
+  		restrict: 'E',
+    	templateUrl: 'partials/partial2.html'
+  	}
+  });
